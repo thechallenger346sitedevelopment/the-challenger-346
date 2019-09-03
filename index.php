a5964b27ad45e572d766c5ee2cfcca7fe3425891
thechallenger346 <45742653+thechallenger346@users.noreply.github.com>
2019-09-03 19:57:08 -0400

    Completely changed the site
    I redid the html files into PHP files since I am going to host them on a raspberry pi. I added a background. I also added the music that was on the original site.

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+ <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <style>
+   body{
+    width: 100%;
+    height: 100%;
+   }
+   .oof{
+    top: 0px;
+    left: 0px;
+    width: 100%;
+    position: absolute;
+    background:yellow;
+    text-align: center;
+    color: red;
+   }
+   .heading-picture{
+    background-image: url("heading-image.jpg");
+    background-position: middle top;
+    background-attachment: scroll;
+    background-repeat: repeat-x;
+    top: 12.8%;
+    width: 100%;
+    height: 25%;
+    left: 0px;
+    position: absolute;
+    text-align: center;
+    color: white;
+    }
+    .page-bottom{
+     background: limegreen;
+     bottom: 0%;
+     width: 100%;
+     height: 10%;
+     left: 0px;
+     position: absolute;
+     text-align: center;
+     color: white;
+     }
+     .TOS{
+      top: 15%;
+      width: 30%;
+      height: 75%;
+      left: 35%;
+      position: absolute;
+      text-align: center;
+      background: red;
+      color: yellow;
+      border: none;
+      }
+  </style>
+  <title>The Challenger 346</title>
+ </head>
+ <body>
+  <div class="oof">
+   <h1>Welcome to THE CHALLNEGER 346  Webpage</h1>
+  </div>
+  <div class="heading-picture">
+   <p>On this page you will see our discord and buttons to other pages and a description of our discord server that the bottom. Please go to the bottom of the page to the footer and read terms of service!</p>
+  </div>
+ </body>
+ <footer class="page-bottom">
+  <a href="terms.php">
+   <button class="TOS">Terms of Service</button>
+  </a>
+ </footer>
+ <audio style="width:0px;height:0px;" autoplay>
+ <source src="music/music.mp3" type="audio/mpeg">
+ <source src="music/music.mp3" type="audio/mpeg">
+Your browser does not support the audio element.
+</audio>
+</html>
---
+++ terms.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+ <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <style>
+   body{
+    width: 100%;
+    height: 100%;
+   }
+   .oof{
+    top: 0px;
+    left: 0px;
+    width: 100%;
+    position: absolute;
+    background:yellow;
+    text-align: center;
+    color: red;
+   }
+   .heading-picture{
+    background-color: black;
+    background-position: middle top;
+    background-attachment: scroll;
+    background-repeat: repeat-x;
+    top: 12.8%;
+    width: 100%;
+    height: 87.3%;
+    left: 0px;
+    position: absolute;
+    text-align: center;
+    color: red;
+    }
+    button{
+     width: 15%;
+     height: 7%;
+     background: red;
+     color: yellow;
+     border: none;
+    }
+  </style>
+  <title>The Challenger 346</title>
+ </head>
+ <body>
+  <div class="oof">
+   <h1>Terms of Service</h1>
+  </div>
+  <div class="heading-picture">
+   <a href="index.php">
+   <button>HOME</button>
+  </a>
+   <p>This site is meant to only report scammers. IF all the information that is given to this site is false and involves companies or even federal workers or federal companies that infomation will be passed on to those places. We record all data that you report including your IP(Internet Protocol) address. This IP will be sent to the correct people for them to further do what they want to do with it.</p>
+  </div>
+ </body>
+ <audio style="width:0px;height:0px;" autoplay>
+ <source src="music/music.mp3" type="audio/mpeg">
+ <source src="music/music.mp3" type="audio/mpeg">
+Your browser does not support the audio element.
+</audio>
+</html>
