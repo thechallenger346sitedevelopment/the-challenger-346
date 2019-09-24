64a793ddc36f45cd3f738d9e2c837cacf2cca6db
BlueberryTech123 <32250628+BlueberryTech123@users.noreply.github.com>
2019-09-24 16:07:46 -0700

    Tidied up the code, made discord widget smaller
    in addition, the discord widget was centered

--- index.php
+++ index.php
@@ -1,144 +1,166 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
- <head>
-  <meta charset="utf-8">
-  <meta name="keywords" content="Scambaiting, the challenger 346 YouTube, Scambait, Scambaiter,the challenger 346, report a scam, report a scammer, how to report a scam">
-  <meta name="author" content="the challenger 346 team">
-  <meta name="copyright" content="The Challenger 346 is a registered trademark of the challenger 346 youtube">
-  <meta name="url" content="https://thechallenge346.tk">
-  <meta name="description" content="We flood scammers and call them all day so please report them here.">
-  <meta name="subject" content="What do we do?">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <style>
-  @import url('https://fonts.googleapis.com/css?family=Turret+Road&display=swap');
-   body{
-    width: 100%;
-    font-family: 'Turret Road', cursive;
-    background: black;
-   }
-   .oof{
-    top: 0px;
-    left: 0px;
-    width: 100%;
-    height: 35%;
-    position: absolute;
-    background: url("https://cdn.discordapp.com/attachments/546775951401549864/625830211887300608/unknown.png");
-    text-align: center;
-    color: white;
-   }
-   .heading-picture{
-    background: url("https://cdn.discordapp.com/attachments/546775951401549864/625830211887300608/unknown.png");
-    top: 14%;
-    width: 100%;
-    height: 10%;
-    left: 0px;
-    position: absolute;
-    text-align: center;
-    color: white;
-    padding:10px;
-    margin-bottom: 20px;
-    }
-    .page-bottom{
-     background: url("https://cdn.discordapp.com/attachments/546775951401549864/625830211887300608/unknown.png");
-     bottom: 0%;
-     width: 100%;
-     height: 10%;
-     left: 0px;
-     position: absolute;
-     text-align: center;
-     color: white;
-     }
-     .TOS{
-      top: 15%;
-      width: 30%;
-      height: 75%;
-      left: 35%;
-      position: absolute;
-      text-align: center;
-      color: white;
-      border: none;
-      background: rgb(10, 12, 100);
-       border: 3px rgb(10, 12, 100);
-       font-family: 'Turret Road', cursive;
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="keywords" content="Scambaiting, the challenger 346 YouTube, Scambait, Scambaiter,the challenger 346, report a scam, report a scammer, how to report a scam">
+    <meta name="author" content="the challenger 346 team">
+    <meta name="copyright" content="The Challenger 346 is a registered trademark of the challenger 346 youtube">
+    <meta name="url" content="https://thechallenge346.tk">
+    <meta name="description" content="We flood scammers and call them all day so please report them here.">
+    <meta name="subject" content="What do we do?">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+      @import url('https://fonts.googleapis.com/css?family=Turret+Road&display=swap');
+
+      body {
+        width: 100%;
+        font-family: 'Turret Road', cursive;
+        background: black;
       }
-      .discord{
-       background: url("https://cdn.discordapp.com/attachments/546775951401549864/625830211887300608/unknown.png");
-       height: 45%;
-       width: 100%;
+
+      .oof {
+        top: 0px;
+        left: 0px;
+        width: 100%;
+        height: 35%;
+        position: absolute;
+        background: url("https://cdn.discordapp.com/attachments/546775951401549864/625830211887300608/unknown.png");
+        text-align: center;
+        background-attachment: fixed;
+        color: white;
       }
-      .discord1{
-       background: black;
-       bottom: 2%;
-       left: 0px;
-       height: 60%;
-       width: 100%;
-       position: absolute;
-       background:0, 47, 92;
-       text-align: center;
-       color: rgb(120, 0, 0);
+
+      .heading-picture {
+        background: url("https://cdn.discordapp.com/attachments/546775951401549864/625830211887300608/unknown.png");
+        background-attachment: fixed;
+        top: 14%;
+        width: 100%;
+        height: 10%;
+        left: 0px;
+        position: absolute;
+        text-align: center;
+        color: white;
+        padding: 10px;
+        margin-bottom: 20px;
       }
-      .buttons{
-       width: 100%;
-       height: 10%;
-       background-color: url("https://cdn.discordapp.com/attachments/546775951401549864/625830211887300608/unknown.png");
-       position: absolute;
-       left: 0%;
-       top: 30%;
+
+      .page-bottom {
+        background: url("https://cdn.discordapp.com/attachments/546775951401549864/625830211887300608/unknown.png");
+        background-attachment: fixed;
+        bottom: 0%;
+        width: 100%;
+        height: 10%;
+        left: 0px;
+        position: absolute;
+        text-align: center;
+        color: white;
       }
-      .discord-button{
-       top: 15%;
-       width: 20%;
-       height: 40%;
-       left: 40%;
-       position: absolute;
-       text-align: center;
-       background: rgb(10, 12, 100);
-       border: 3px rgb(10, 12, 100);
-       color: white;
-       font-family: 'Turret Road', cursive;
+
+      .TOS {
+        top: 15%;
+        width: 30%;
+        height: 75%;
+        left: 35%;
+        position: absolute;
+        text-align: center;
+        color: white;
+        border: none;
+        background: rgb(10, 12, 100);
+        border: 3px rgb(10, 12, 100);
+        font-family: 'Turret Road', cursive;
       }
-  </style>
-  <title>The Challenger 346</title>
- </head>
- <!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/5cf881ccb534676f32ad91e7/default';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
-<!--End of Tawk.to Script-->
- <body>
-  <div class="oof">
-   <h1>Welcome to THE CHALLNEGER 346</h1>
-  </div>
-  <div class="heading-picture">
-   <p>On this page you will see our discord and buttons to other pages and a description of our discord server that the bottom. Please go to the bottom of the page to the footer and read terms of service!</p>
-  </div>
-  <div class="buttons">
-   <a href="https://discord.gg/EAMgjte" target="_blank">
-   <button class="discord-button">Discord Server</button>
-  </a>
-  </div>
-  <div class="discord">
-   <div class="discord1">
-   <iframe src="https://discordapp.com/widget?id=528007140900732938&theme=dark" width="100%" height="100%" allowtransparency="true" frameborder="0"></iframe>
-   </div>
-  </div>
- </body>
- <footer class="page-bottom">
-  <a href="terms.php">
-   <button class="TOS">Terms of Service</button>
-  </a>
- </footer>
- <audio style="width:0px;height:0px;" loop autoplay>
- <source src="music/music.mp3" type="audio/mpeg">
- <source src="music/music.mp3" type="audio/mpeg">
-Your browser does not support the audio element.
-</audio>
+
+      .discord {
+        background: url("https://cdn.discordapp.com/attachments/546775951401549864/625830211887300608/unknown.png");
+        height: 45%;
+        width: 100%;
+        left: 50%;
+        right: 50%;
+      }
+
+      .discord1 {
+        background: black;
+        bottom: 2%;
+        left: 0px;
+        height: 60%;
+        width: 40%;
+        position: absolute;
+        background: 0, 47, 92;
+        text-align: center;
+        color: rgb(120, 0, 0);
+        left: 30%;
+      }
+
+      .buttons {
+        width: 100%;
+        height: 10%;
+        background: url("https://cdn.discordapp.com/attachments/546775951401549864/625830211887300608/unknown.png");
+        position: absolute;
+        left: 0%;
+        top: 10%;
+      }
+
+      .discord-button {
+        top: 15%;
+        width: 20%;
+        height: 40%;
+        left: 40%;
+        position: absolute;
+        text-align: center;
+        background: rgb(10, 12, 100);
+        border: 3px rgb(10, 12, 100);
+        color: white;
+        font-family: 'Turret Road', cursive;
+      }
+
+    </style>
+    <title>The Challenger 346</title>
+  </head>
+  <!--Start of Tawk.to Script-->
+  <script type="text/javascript">
+    var Tawk_API = Tawk_API || {},
+      Tawk_LoadStart = new Date();
+    (function() {
+      var s1 = document.createElement("script"),
+        s0 = document.getElementsByTagName("script")[0];
+      s1.async = true;
+      s1.src = 'https://embed.tawk.to/5cf881ccb534676f32ad91e7/default';
+      s1.charset = 'UTF-8';
+      s1.setAttribute('crossorigin', '*');
+      s0.parentNode.insertBefore(s1, s0);
+    })();
+
+  </script>
+  <!--End of Tawk.to Script-->
+
+  <body>
+    <div class="oof">
+      <h1>Welcome to THE CHALLNEGER 346</h1>
+    </div>
+    <div class="heading-picture">
+      <p>On this page you will see our discord and buttons to other pages and a description of our discord server that the bottom. Please go to the bottom of the page to the footer and read terms of service!</p>
+    </div>
+    <div class="buttons">
+      <a href="https://discord.gg/EAMgjte" target="_blank">
+        <button class="discord-button">Join Discord</button>
+      </a>
+    </div>
+    <div class="discord" align="center">
+      <div class="discord1">
+        <iframe src="https://discordapp.com/widget?id=528007140900732938&theme=dark" width="100%" height="100%" allowtransparency="true" frameborder="0" class="discordwidget"></iframe>
+      </div>
+    </div>
+  </body>
+  <footer class="page-bottom">
+    <a href="terms.php">
+      <button class="TOS">Terms of Service</button>
+    </a>
+  </footer>
+  <audio style="width:0px;height:0px;" loop autoplay>
+    <source src="music/music.mp3" type="audio/mpeg">
+    <source src="music/music.mp3" type="audio/mpeg">
+    Your browser does not support the audio element.
+  </audio>
+
 </html>
